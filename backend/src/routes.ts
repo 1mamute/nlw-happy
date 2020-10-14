005aca5c1cef372bf327082801a0b4d2bf930fc3
thelastmmt <gabrielbassinello@hotmail.com>
2020-10-14 20:30:47 -0300

    Abstraimos um pouco a API criando controller para gerencias as requisições HTTP. create e index já estão funcionando ok, checado no insomnia

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,38 +1,9 @@
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
 import { Router } from 'express';
+import OrphanagesControllers from './controllers/OrphanagesControllers';
 
 const routes = Router();
 
-routes.post('/orphanages', async (request, response) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  } = request.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  console.log('Sending post /orphanages!');
-  console.log(request.body);
-
-  return response.status(201).json({ message: 'Hello world' });
-});
+routes.get('/orphanages', OrphanagesControllers.index);
+routes.post('/orphanages', OrphanagesControllers.create);
 
 export default routes;
