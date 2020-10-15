6b4849f12ad129780d4a9cf8e80056498ecddd48
thelastmmt <gabrielbassinello@hotmail.com>
2020-10-15 13:10:33 -0300

    Adicionado assets da aula 3, configuração inicial de markers e popups no mapa

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
 
 import '../styles/pages/orphanages-map.css';
 
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
+
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/Map-Marker.svg';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 export default function OrphanagesMap() {
   return (
@@ -31,6 +40,19 @@ export default function OrphanagesMap() {
         zoom={13.5}
         style={{ width: '100%', height: '100%' }}
       >
+        <Marker position={[-22.7146356, -47.6218322]} icon={mapIcon}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Lar dos velhinhos
+            <Link to="">
+              <FiArrowRight size={20} color="#FFF" />
+            </Link>
+          </Popup>
+        </Marker>
         <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       </Map>
 
