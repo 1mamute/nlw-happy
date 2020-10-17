7ed817bea9c025a9148c69f144086a8880362176
thelastmmt <gabrielbassinello@hotmail.com>
2020-10-16 21:08:36 -0300

    Importação do react-native-maps, configuração inicial do mapa na tela. Tive que importar o módulo Text do react-native se não o typescript reclamava do styles.calloutText

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>IS working? to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import mapMarker from './src/images/MapMarker.png';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +11,52 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -22.7146356,
+          longitude: -47.6218322,
+          latitudeDelta: 0.006,
+          longitudeDelta: 0.006,
+        }}>
+        <Marker
+          icon={mapMarker}
+          coordinate={{
+            latitude: -22.7146356,
+            longitude: -47.6218322,
+          }}>
+          <Callout tooltip>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar do gabriel</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+    </View>
+  );
+}
